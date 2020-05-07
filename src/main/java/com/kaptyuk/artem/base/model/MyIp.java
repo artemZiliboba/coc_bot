33f3e57d45d4098fec83f331bc10ead5057cf01c
Artem.Kaptyuk <Artem.Kaptyuk@nexign.com>
2020-05-07 22:24:16 +0300

    First commit

---
+++ src/main/java/com/kaptyuk/artem/base/model/MyIp.java
@@ -0,0 +1,14 @@
+package com.kaptyuk.artem.base.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MyIp {
+    private String ip;
+}
---
+++ src/main/java/com/kaptyuk/artem/base/service/ITelegramApi.java
@@ -0,0 +1,7 @@
+package com.kaptyuk.artem.base.service;
+
+import com.kaptyuk.artem.base.model.telegram.me.Me;
+
+public interface ITelegramApi {
+    Me getMe();
+}
