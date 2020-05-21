1d288064a0e04f8943ab96ebb0401eb96611d699
Artem.Kaptyuk <Artem.Kaptyuk@nexign.com>
2020-05-21 22:52:18 +0300

    Delete unique from field tag_clan

--- src/main/java/com/home/server/db/HerokuPostgresql.java
+++ src/main/java/com/home/server/db/HerokuPostgresql.java
@@ -51,7 +51,7 @@ public class HerokuPostgresql {
             statement.executeUpdate("create table if not exists COC.PLR(plr_id serial primary key, " +
                     "username varchar (50) not null, " +
                     "tag varchar (50) unique not null, " +
-                    "tag_clan varchar (50) unique, " +
+                    "tag_clan varchar (50), " +
                     "trophies integer, " +
                     "vs_trophies integer, " +
                     "th integer)"
